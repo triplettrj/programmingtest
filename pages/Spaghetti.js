4a2981bc2a85526be5a5bf69bf81584794fe590e
Ryry Triplett <triplettrj@gmail.com>
2022-09-24 17:26:33 +0700

    used chartjs to plot spaggehti diagram

--- pages/Spaghetti.js
+++ pages/Spaghetti.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import {supabase} from '../database/Database'
+import Spaghettidiagram from './components/spaghettidiagram'
 
 
 function Spaghetti(props) {
   const [backgroundimageUrl, setBackgroundimageUrl] = useState("")
+
+
 
   useEffect(() => {
     const getData = async () => {
       const { data, error } = await supabase.from('profiles').select() 
       setBackgroundimageUrl(data[0].avatar_url)
       if (error) alert('you got an error here it is ,', error)
+      
     }
     getData()
   }, [])
 
   return (
     <div>
+      <Spaghettidiagram style={{width: "500"}}/>
       {backgroundimageUrl ? <img src={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} width={1000} alt="backgroundImage"/> : "No Background Image set"}
     </div>
   );
