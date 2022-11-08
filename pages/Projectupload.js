aad1103238899d1b73f7fda9b7e0e02d421278d6
Ryry Triplett <triplettrj@gmail.com>
2022-11-08 17:01:54 +0700

    changed next/Link to next/link

--- pages/Projectupload.js
+++ pages/Projectupload.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {supabase} from '../database/Database'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 function Projectupload() {
   const [image, setImage] = useState(null)
