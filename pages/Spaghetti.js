09783455126dae966db43ccfd82a374c94684636
Ryry Triplett <triplettrj@gmail.com>
2023-01-09 18:49:43 +0700

    setup for stopping point

--- pages/Spaghetti.js
+++ pages/Spaghetti.js
@@ -1,64 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import {supabase} from '../database/Database'
 import Spaghettidiagram from './components/spaghettidiagram'
-const CSVToJSON = require("csvtojson")
-
-
-
-// export const getStaticProps = async () => {
-//   let datalogUrl = "https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/datalog.cvs?t=2022-11-04T09%3A49%3A47.092Z"  
-//   const getData = async () => {
-//     let { data, error } = await supabase.from('profiles').select() 
-//     datalogUrl = datalogUrl + data[0].datalogUrl
-//     console.log('this is datalogurl ', datalogUrl)
-//     if (error) alert('you got an error here it is ,', error)
-//   }
-//   getData()
-//   console.log('this is datalogurl ', datalogUrl)
-
-//   const jsonAry = await CSVToJSON()
-//     //.fromStream(request.get(datalogUrl))
-//     .fromStream(request.get('https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/datalog.cvs?t=2022-11-04T09%3A49%3A47.092Z'))
-//   return {
-//     props : {data : jsonAry || [], fallback: false }
-//   }
-// }
 
 function Spaghetti(props) {
   const [backgroundimageUrl, setBackgroundimageUrl] = useState("")
   const [datalogUrl, setDatalogUrl] = useState("")
   const [datalog, setDatalog] = useState([])
-  const [backgroundimageX, setBackgroundimageX] = useState(null)
-  const [backgroundimageY, setBackgroundimageY] = useState(null)
   const [data, setData] = useState(null)
-
-  const handleBackgroundimageClick = (e) => {
-    setBackgroundimageX(e.nativeEvent.offsetX)
-    setBackgroundimageY(e.nativeEvent.offsetY)
-  }
-
-  const inlinestyle = {
-    position: "relative",
-    left: `${backgroundimageX}px`,
-    top: `${backgroundimageY}px`,
-    zindex: -1,
-  }
-
-  const picturestyle = {
-    width: '500px',
-    height: '500px',
-    backgroundimageUrl: 'https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/Programming_Test_layout.png',
-  }
-
 
   useEffect(() => {
     const getData = async () => {
       const { data, error } = await supabase.from('profiles').select() 
-
       console.log('this is data', data)
       console.log('this is datalogUrl', data[0].datalogUrl) //send this to spaghettidiaroma component
       setData(data)
-
     if (error) alert('you got an error here it is ,', error)
     }
     getData()
@@ -67,48 +22,8 @@ function Spaghetti(props) {
   return (
     <div>
       <Spaghettidiagram data={data} />
-
-      {backgroundimageUrl ? 
-      <div className="container">
-      <picture>
-        <source  
-          srcSet={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} 
-          type="image/webp" 
-        />
-        <img 
-          src={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} 
-          alt="backgroundImage"
-          onClick={handleBackgroundimageClick} 
-        />
-      </picture>
-      </div>
-      : "No Background Image set"}
-
-      <div className="container">
-        <div className="pictureouside" onClick={handleBackgroundimageClick}> 
-          <div style={inlinestyle}>
-            o   
-          <input type="text" placeholder='Title' />
-          <input type="text" placeholder='Duration' />
-          </div>  
-        </div>
-      </div>
     </div>
   )
 }
 
 export default Spaghetti
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const { data, error } = await supabase.storage
-  //     .from('avatars')
-  //     .download('1664435228485_datalog.cvs')      //cant get .download(datalogUrl.split('/')[1]) to work.....
-  //     //data.text().then(text => setDatalog(text))
-  //     //console.log(data)
-
-  //     //console.log('this is the datalog, ', datalog)
-  //     if (error) console.log('error on getting datalog, ', error)
-  //   }
-  //   getData()
-  // }, [datalogUrl])
--- pages/components/spaghettidiagram.js
+++ pages/components/spaghettidiagram.js
@@ -8,6 +8,8 @@ function Spaghettidiagram({data}) {
   const [csvData, setCsvData] = useState([])
   const [dataSetSpag, setDataSetSpag] = useState([])
   const [clickedPoint, setClickedPoint] = useState(null)
+  const [backgroundimageX, setBackgroundimageX] = useState(null)
+  const [backgroundimageY, setBackgroundimageY] = useState(null)
 
   useEffect(() => {
     fetch('https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/1667958550823_Johanna%20(1)%20-%20Copy.csv')
@@ -42,7 +44,6 @@ function Spaghettidiagram({data}) {
           complete: results => {
             // Convert the parsed data to JSON and store it in state
             setCsvData(results.data);
-            console.log('this is cvsData', csvData)
           }
         })
       })
@@ -79,6 +80,7 @@ function Spaghettidiagram({data}) {
         pointBackgroundColor: 'rgba(255, 99, 132, 1)',
         pointHoverBorderWidth: 0,
       },
+      //stopping point option 1: click on scatter and add new data set
       {
         label: '',
         data: [clickedPoint],
@@ -131,26 +133,49 @@ function Spaghettidiagram({data}) {
     }
     
   }
-  
+
+  //stopping point option 2: click on div container
+  const inlinestyle = {
+    position: "relative",
+    //left: `${backgroundimageX}px`,
+    //top: `${backgroundimageY}px`,
+    zindex: -1,
+  }
+
+  const handleBackgroundimageClick = (e) => {
+    setBackgroundimageX(e.nativeEvent.offsetX)
+    setBackgroundimageY(e.nativeEvent.offsetY)
+  }
+
   if(data){
     console.log('this is datalogUrl on spag comp', data[0].datalogUrl)
     console.log('this is avatar url on spag comp', data[0].avatar_url)
   }
 
-
-  
-  
   return (
     <div  >
-      {data[0].avatar_url ? <Scatter
-        data={chartData}
-        options={chartOptions}
-        //onClick={handleClick}
-        style={{
-          backgroundImage: `url(https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${data[0].avatar_url}`,
-          //height: '400px',
-        }}
-      /> : ""}
+      {data ? 
+      <>
+      <div>
+        <Scatter
+          data={chartData}
+          options={chartOptions}
+          //onClick={handleClick}
+          style={{backgroundImage: `url(https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${data[0].avatar_url}`,}}
+        />
+        <div  onClick={handleBackgroundimageClick}> 
+          <div style={inlinestyle}>
+            o   
+          <input type="text" placeholder='Title' />
+          <input type="text" placeholder='Duration' />
+          <button type={"submit"}>Submit</button>
+          </div>  
+        </div>
+      </div>
+      
+
+      </>
+      : ""}
     </div>
       
 
