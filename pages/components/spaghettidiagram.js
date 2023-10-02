9595b9a2ebd24bc8315ea7abf1d87f2f64b405b2
Ryry Triplett <triplettrj@gmail.com>
2023-10-02 12:30:52 +0700

    deleted semicolon, covered chatgpt tracks

--- pages/components/spaghettidiagram.js
+++ pages/components/spaghettidiagram.js
@@ -62,7 +62,7 @@ function Spaghettidiagram({data}) {
   }, [cvsDataurl])
 
 function convertArray(arrayOfArrays) {
-  const keys = ["timestamp", "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10", "value11", "value12", "value13", "value14", "value15", "value16"];
+  const keys = ["timestamp", "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10", "value11", "value12", "value13", "value14", "value15", "value16"]
   const arrayOfObjects = arrayOfArrays.map((array) => {
       const object = {}
       array.forEach((value, index) => {
