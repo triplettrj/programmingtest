4f526b30338c7f8ab0127a3212ab94074f280d84
Ryry Triplett <triplettrj@gmail.com>
2023-10-04 11:07:41 +0700

    clean up code a little

--- pages/Projectupload.js
+++ pages/Projectupload.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {supabase} from '../database/Database'
+import { supabase } from '../database/Database'
 import Link from 'next/link'
 import jwt_decode from 'jwt-decode'
 import styles from '../styles/form.module.css'
@@ -11,69 +11,68 @@ function Projectupload() {
   const [projectTitle, setProjectTitle] = useState("")
   const [userid, setUserid] = useState(null)
   const [showBackgroundImagePreview, setShowBackgroundImagePreview] = useState(false)
-  const [showLogFilePreview, setShowLogFilePreview] = useState(false);
-  const [logFileContent, setLogFileContent] = useState(""); 
-  
+  const [showLogFilePreview, setShowLogFilePreview] = useState(false)
+  const [logFileContent, setLogFileContent] = useState("")
+
   const handleShowLogFilePreview = async () => {
     if (!showLogFilePreview) {
       const logFileUrl =
-        'https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/1673426274689_2021_09_15__1352__marvelmind.csv';
-  
+        'https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/1673426274689_2021_09_15__1352__marvelmind.csv'
+
       try {
-        const response = await fetch(logFileUrl);
-        const text = await response.text();
-        const rows = text.split('\n').map((row) => row.split(',')); // Assuming CSV format
-  
+        const response = await fetch(logFileUrl)
+        const text = await response.text()
+        const rows = text.split('\n').map((row) => row.split(',')) // Assuming CSV format
+
         // Update the log file content state variable
-        setLogFileContent(rows);
+        setLogFileContent(rows)
       } catch (error) {
-        console.error('Error loading log file:', error);
-        setLogFileContent([]); // Set content to an empty array in case of an error
+        console.error('Error loading log file:', error)
+        setLogFileContent([]) // Set content to an empty array in case of an error
       }
     }
-  
-    setShowLogFilePreview(!showLogFilePreview);
-  };
-  
+
+    setShowLogFilePreview(!showLogFilePreview)
+  }
 
   useEffect(() => {
     const tempid = jwt_decode(window.localStorage.getItem('supabase.auth.token')).sub
     setUserid(tempid)
-  },[]) 
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     let backgroundimageUrl = ""
     let datalogUrl = ""
 
-      if(image) {
-        const {data, error} = await supabase.storage.from("avatars").upload(`${Date.now()}_${image.name}`, image)
-        if(error) alert(error.message)
-        if(data) {
-          setBackgroundimageUrl(data.Key)
-          backgroundimageUrl = data.Key
-          alert('Background Image upload successful')
-        }
+    if (image) {
+      const { data, error } = await supabase.storage.from("avatars").upload(`${Date.now()}_${image.name}`, image)
+      if (error) alert(error.message)
+      if (data) {
+        setBackgroundimageUrl(data.Key)
+        backgroundimageUrl = data.Key
+        alert('Background Image upload successful')
       }
-
-      if(datalog) {
-        const {data, error} = await supabase.storage.from("avatars").upload(`${Date.now()}_${datalog.name}`, datalog)
-        if(error) alert(error.message)
-        if(data) {
-          datalogUrl = data.Key
-          alert('Datalog upload successful')
-        }
-      }
-
-      const {data, error} = await supabase.from("profiles").upsert({
-        id: userid, 
-        project_title: projectTitle,
-        avatar_url: backgroundimageUrl,
-        datalogUrl: datalogUrl,
-      })
-      if(error) alert(error.message)
     }
-    
+
+    if (datalog) {
+      const { data, error } = await supabase.storage.from("avatars").upload(`${Date.now()}_${datalog.name}`, datalog)
+      if (error) alert(error.message)
+      if (data) {
+        datalogUrl = data.Key
+        alert('Datalog upload successful')
+      }
+    }
+
+    const { data, error } = await supabase.from("profiles").upsert({
+      id: userid,
+      project_title: projectTitle,
+      avatar_url: backgroundimageUrl,
+      datalogUrl: datalogUrl
+    })
+    if (error) alert(error.message)
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className={styles.container}>
@@ -111,7 +110,7 @@ function Projectupload() {
           </button>
         </div>
       </form>
-      
+
       <div className={styles.note}>
         <div>
           <a href="https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/avatars/1673426268457_lunchrum%20(2).png">
@@ -165,7 +164,7 @@ function Projectupload() {
           />
         </div>
       )}
-      
+
       <div>
         {backgroundimageUrl ? (
           <Link href="/Spaghetti">
@@ -182,14 +181,17 @@ function Projectupload() {
         {projectTitle ? <h1>{projectTitle}</h1> : ""}
       </div>
       <div>
-        {backgroundimageUrl ? 
-        <picture>
-          <source srcSet={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} type="image/webp" />
-          <img src={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} alt="backgroundImage" />
-        </picture>
-        : ""}
+        {backgroundimageUrl ?
+          <picture>
+            <source srcSet={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} type="image/webp" />
+            <img src={`https://irqserdsvujcsqwnmndt.supabase.co/storage/v1/object/public/${backgroundimageUrl}`} alt="backgroundImage" />
+          </picture>
+          : ""}
       </div>
     </>
-)}
+  )
+}
+
+
 
 export default Projectupload
