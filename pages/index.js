db7216ae659b3c0584abd750093bd20c2d8f0042
Ryry Triplett <triplettrj@gmail.com>
2023-10-19 16:15:19 +0700

    added loading component to index.js and countdown is working

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import Loading from './components/loading'
 
 export default function Home() {
-  return (
-    <> 
-      <div>yoooooo</div>
-      <a href="Loginpage">Loginpage</a>
-    </>
+  const [isLoading, setIsLoading] = useState(true);
 
-  )
+  // Simulate a loading time (e.g., 5 seconds)
+  useEffect(() => {
+    // Simulate an asynchronous operation (e.g., data fetching)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 6000); // Adjust the time as needed
+
+    return () => {
+      // Cleanup if needed
+    };
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading /> // Display the Loading component while loading
+      ) : (
+        <>
+          <div>yoooooo</div>
+          <a href="Loginpage">Loginpage</a>
+        </>
+      )}
+    </>
+  );
 }
