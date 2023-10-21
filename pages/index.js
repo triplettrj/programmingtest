db96e3f925e869449fc610ff4a398f5df4284de2
Ryry Triplett <triplettrj@gmail.com>
2023-10-21 21:19:59 +0700

    reverted to not abstracted useCoountdown bc deployment failed on vercel

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './components/loading'
-import useCountdown from './components/useCountdown'
+//import useCountdown from './components/useCountdown'
 
 export default function Home() {
-  const isLoading = useCountdown(true)
+  const [isLoading, setIsLoading] = useState(true)
+
+  // Simulate a loading time 
+  useEffect(() => {
+    // Simulate an asynchronous operation 
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000) // Adjust the time as needed
+
+    return () => {
+      // Cleanup if needed
+    }
+  }, [])
 
   return (
     <>
@@ -16,5 +28,5 @@ export default function Home() {
         </>
       )}
     </>
-  );
+  )
 }
