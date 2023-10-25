35504f7dfdcf810ad73753fce898375dbab22a0c
Ryry Triplett <triplettrj@gmail.com>
2023-10-25 14:14:14 +0700

    removed useCountdown hook

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Loading from './components/loading'
-import useCountdown from './components/useCountdown'
 import Layout from './components/layout'
 
 export default function Home() {
-  const isCountdowning = useCountdown(true)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulate an asynchronous operation
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false)
+    }, 5000) // Adjust the time as needed
+
+    // Clean up the timeout when the component unmounts
+    return () => {
+      clearTimeout(loadingTimeout)
+    }
+  }, [])
 
   return (
-
-      <>
-      {isCountdowning ? (
-        <Loading /> // Display the Loading component while counting down
+    <>
+      {isLoading ? (
+        <Loading /> // Display the Loading component while loading
       ) : (
         <Layout>
           Click on a link and live your dreams
         </Layout>
       )}
     </>
-
-    
   )
 }
