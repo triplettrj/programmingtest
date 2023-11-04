836d9be537fab5e560a2b222a92853191857f1bc
Ryry Triplett <triplettrj@gmail.com>
2023-11-04 13:23:01 +0700

    content for preview component

--- pages/components/preview.js
+++ pages/components/preview.js
@@ -1,11 +1,11 @@
 import React from 'react'
-//import LoadingIndicator from './LoadingIndicator'
+import LoadingIndicator from './loadingIndicator'
 
 function Preview({ show, isLoading, content }) {
   return show ? (
     <div>
-      {/* Add logic to display previews, e.g., image or table */}
-      {/*<LoadingIndicator isLoading={isLoading} />*/}
+      {content}
+      <LoadingIndicator isLoading={isLoading} />
     </div>
   ) : null
 }
