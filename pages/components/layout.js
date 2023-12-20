cec4eb112b5b4a3dc6f471e7cbe58f2237f8c715
Ryry Triplett <triplettrj@gmail.com>
2023-12-20 12:59:51 +0700

    Refactor navigation links in layout component

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -2,20 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../../styles/layout.module.css'
 
+const navLinks = [
+  { href: "/", text: "Home" },
+  { href: "/Loginpage", text: "Login" },
+  { href: "/Projectupload", text: "Project Upload" },
+  { href: "/Spaghetti", text: "Spaghetti diagram" },
+]
+
 const Layout = ({ children }) => {
-return (
+  return (
     <div className={styles['layout-container']}>
-    <header className={styles.container}>
+      <header className={styles.container}>
         <nav>
-        <Link href="/">Home</Link>
-        <Link href="/Loginpage">Login</Link>
-        <Link href="/Projectupload">Project Upload</Link>
-        <Link href="/Spaghetti">Spaghetti diagram</Link>
+          {navLinks.map((link, index) => (
+            <Link key={index} href={link.href}>{link.text}</Link>
+          ))}
         </nav>
-    </header>
-    <main>{children}</main>
+      </header>
+      <main>{children}</main>
     </div>
-    )
+  )
 }
 
 export default Layout
